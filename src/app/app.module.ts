ea65d2a4f2b7c7f2a2a42d6ca672eb4d8b9028d7
yogaggar <yogaggar@publicissapient.com>
2021-12-06 18:11:40 +0530

    Checkpoint for app module -> routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Core/Components/navbar/navbar.component';
+import { CardComponent } from './Core/Components/card/card.component';
+import { TableComponent } from './Core/Components/table/table.component';
+import { ButtonComponent } from './Core/Components/buttons/button.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { AddPatientComponent } from './patient/add-patient/add-patient.component';
+import { PatientComponent } from './patient/patient.component';
+import { EditPatientComponent } from './patient/edit-patient/edit-patient.component';
+import { ViewPatientComponent } from './patient/view-patient/view-patient.component';
+import { PatientListingComponent } from './modules/patient-listing/patient-listing.component';
+import { ClinicListingComponent } from './clinic-listing/clinic-listing.component';
+import { PageHeaderComponent } from './Core/Components/page-header/page-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    TableComponent,
+    DashboardComponent,
+    ButtonComponent,
+    AddPatientComponent,
+    PatientComponent,
+    EditPatientComponent,
+    ViewPatientComponent,
+    PatientListingComponent,
+    ClinicListingComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
