f6a20430c21cc72a931f2e0084f0f27059f3cb22
yogaggar <yogaggar@publicissapient.com>
2021-12-07 22:26:04 +0530

    Add loader in dashboard

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,15 +1,14 @@
-import { HelperUtil } from './../../Core/Utility/helper-utility';
-import { RequestUtil } from '../../Core/Utility/request-utility';
-import { IRowDataConfig } from './../../Core/interfaces/table.interface';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { map } from 'rxjs/operators';
 import {
   BUTTON_IDs,
   PAGE_URLs,
   TABLE_IDs,
 } from 'src/app/Core/Config/global-config';
 import { PlatformService } from 'src/app/Core/Services/platform.service';
+import { RequestUtil } from '../../Core/Utility/request-utility';
+import { IRowDataConfig } from './../../Core/interfaces/table.interface';
+import { HelperUtil } from './../../Core/Utility/helper-utility';
 
 @Component({
   selector: 'app-dashboard',
@@ -19,12 +18,18 @@ import { PlatformService } from 'src/app/Core/Services/platform.service';
 export class DashboardComponent implements OnInit {
   buttonIds = { ...BUTTON_IDs };
   tableIds = { ...TABLE_IDs };
-  pendingPatientsData: IRowDataConfig[];
-  followUpPatientsData: IRowDataConfig[];
+  pendingPatients: { data: IRowDataConfig[]; isLoading: boolean } = {
+    data: [],
+    isLoading: true,
+  };
+  followUpPatients: { data: IRowDataConfig[]; isLoading: boolean } = {
+    data: [],
+    isLoading: true,
+  };
   categoriesMasterData = new Map();
   patientsMasterData = new Map();
-  categoryKey: 'categoryId';
-  patientkey: 'patientId';
+  categoryKey = 'categoryId';
+  patientkey = 'patientId';
 
   constructor(
     private readonly router: Router,
@@ -60,33 +65,43 @@ export class DashboardComponent implements OnInit {
    */
   getPendingPatients(): void {
     const requestBody = RequestUtil.pendingPatients(false, {}, 5);
-    this.platformService.getPendingPatients(requestBody).subscribe((res) => {
-      HelperUtil.createMapFromArray(
-        res.patientDetails,
-        this.patientkey,
-        this.patientsMasterData
-      );
 
-      this.pendingPatientsData = res.pendingPatients.map(
-        (details: any): IRowDataConfig => {
-          return {
-            rowData: {
-              ...details,
-              name: this.patientsMasterData.get(details[this.patientkey]).name,
-              age: this.patientsMasterData.get(details[this.patientkey]).age
-                .value,
-              gender: this.patientsMasterData.get(details[this.patientkey])
-                .gender,
-              category: this.categoriesMasterData.get(details[this.categoryKey])
-                .type,
-              phoneNumber: this.patientsMasterData.get(details[this.patientkey])
-                .phoneNumber.primary,
-            },
-            id: details.patientId,
-          };
-        }
-      );
-    });
+    this.platformService.getPendingPatients(requestBody).subscribe(
+      (res) => {
+        HelperUtil.createMapFromArray(
+          res.patientDetails,
+          this.patientkey,
+          this.patientsMasterData
+        );
+
+        this.pendingPatients.data = res.pendingPatients.map(
+          (details: any): IRowDataConfig => {
+            console.log(details[this.patientkey]);
+            return {
+              rowData: {
+                ...details,
+                name: this.patientsMasterData.get(details[this.patientkey])
+                  .name,
+                age: this.patientsMasterData.get(details[this.patientkey]).age,
+                gender: this.patientsMasterData.get(details[this.patientkey])
+                  .gender,
+                category: this.categoriesMasterData.get(
+                  details[this.categoryKey]
+                ).type,
+                phoneNumber: this.patientsMasterData.get(
+                  details[this.patientkey]
+                ).phoneNumber.primary,
+              },
+              id: details.patientId,
+            };
+          }
+        );
+        this.pendingPatients.isLoading = false;
+      },
+      (err) => {
+        this.pendingPatients.isLoading = false;
+      }
+    );
   }
 
   /**
@@ -94,38 +109,46 @@ export class DashboardComponent implements OnInit {
    */
   getFollowUpPatients(): void {
     const requestBody = RequestUtil.followUpPatients(false, {}, 5);
-    this.platformService.getFollowUpPatients(requestBody).subscribe((res) => {
-      HelperUtil.createMapFromArray(
-        res.patientDetails,
-        this.patientkey,
-        this.patientsMasterData
-      );
+    this.platformService.getFollowUpPatients(requestBody).subscribe(
+      (res) => {
+        HelperUtil.createMapFromArray(
+          res.patientDetails,
+          this.patientkey,
+          this.patientsMasterData
+        );
 
-      this.followUpPatientsData = res.followUpPatients.map(
-        (details: any): IRowDataConfig => {
-          return {
-            rowData: {
-              dateOfVisit: HelperUtil.getStringifiedDate(
-                details.dateOfVisit,
-                true
-              ),
-              followUpDate: HelperUtil.getStringifiedDate(
-                details.followUpDate,
-                true
-              ),
-              name: this.patientsMasterData.get(details[this.patientkey]).name,
-              age: this.patientsMasterData.get(details[this.patientkey]).age
-                .value,
-              category: this.categoriesMasterData.get(details[this.categoryKey])
-                .type,
-              phoneNumber: this.patientsMasterData.get(details[this.patientkey])
-                .phoneNumber.primary,
-            },
-            id: details.patientId,
-          };
-        }
-      );
-    });
+        this.followUpPatients.data = res.followUpPatients.map(
+          (details: any): IRowDataConfig => {
+            return {
+              rowData: {
+                dateOfVisit: HelperUtil.getStringifiedDate(
+                  details.dateOfVisit,
+                  true
+                ),
+                followUpDate: HelperUtil.getStringifiedDate(
+                  details.followUpDate,
+                  true
+                ),
+                name: this.patientsMasterData.get(details[this.patientkey])
+                  .name,
+                age: this.patientsMasterData.get(details[this.patientkey]).age,
+                category: this.categoriesMasterData.get(
+                  details[this.categoryKey]
+                ).type,
+                phoneNumber: this.patientsMasterData.get(
+                  details[this.patientkey]
+                ).phoneNumber.primary,
+              },
+              id: details.patientId,
+            };
+          }
+        );
+        this.followUpPatients.isLoading = false;
+      },
+      (err) => {
+        this.followUpPatients.isLoading = false;
+      }
+    );
   }
 
   /**
@@ -142,4 +165,3 @@ export class DashboardComponent implements OnInit {
     this.router.navigate([...PAGE_URLs.allPatients]);
   }
 }
-
