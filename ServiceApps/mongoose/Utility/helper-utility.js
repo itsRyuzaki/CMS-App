04ed4a457293d2c540b9e857dae85ea2287238f2
yogaggar <yogaggar@publicissapient.com>
2021-12-07 22:23:49 +0530

    Update helper util

--- ServiceApps/mongoose/Utility/helper-utility.js
+++ ServiceApps/mongoose/Utility/helper-utility.js
@@ -10,3 +10,14 @@ export const generateErrorDetails = (response, err) => {
     logs: err,
   };
 };
+
+export const getDueAmount = (fees, paidAmount) => {
+  let totalFees = 0;
+  fees.forEach((fee) => {
+    totalFees += fee.value;
+  });
+  paidAmount.forEach((amount) => {
+    totalFees -= amount.value;
+  });
+  return totalFees;
+};
