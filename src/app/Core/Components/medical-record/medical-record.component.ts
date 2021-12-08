3825bbf67bc8b9a5ab25059438eba0a118293607
yogaggar <yogaggar@publicissapient.com>
2021-12-08 22:18:15 +0530

    Add medical record component

---
+++ src/app/Core/Components/medical-record/medical-record.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IMedicalRecord } from './../../interfaces/api-response.interface';
+
+@Component({
+  selector: 'app-medical-record',
+  templateUrl: './medical-record.component.html',
+  styleUrls: ['./medical-record.component.scss']
+})
+export class MedicalRecordComponent implements OnInit {
+
+  @Input() recordConfig: IMedicalRecord;
+  @Input() category;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
