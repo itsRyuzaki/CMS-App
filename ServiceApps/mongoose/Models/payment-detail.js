c9a4dda9207dda1ac8d1df61b259ec2ed2a8c57c
yogaggar <yogaggar@publicissapient.com>
2021-12-09 16:33:05 +0530

    Update payment model

--- ServiceApps/mongoose/Models/payment-detail.js
+++ ServiceApps/mongoose/Models/payment-detail.js
@@ -1,23 +1,12 @@
 import mongoose from "mongoose";
 const { model, Schema } = mongoose;
-import { COLLECTION_NAMES } from "../Config/global-config.js";
+import { COLLECTION_NAMES, DATE_SCHEMA } from "../Config/global-config.js";
 
 const PaymentDetailSchema = new Schema({
   patientId: String,
   categoryId: String,
-  fees: [
-    {
-      value: Number,
-      date: String,
-    },
-  ],
-  paidAmount: [
-    {
-      value: Number,
-      date: String,
-    },
-  ],
-  dueAmount: Number
+  transactions: [{ fees: Number, paidAmount: Number, date: DATE_SCHEMA }],
+  dueAmount: Number,
 });
 
 export const PaymentDetailModel = new model(
