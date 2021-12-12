cb9526676729b06b62338f179ec07508b932e02d
yogaggar <yogaggar@publicissapient.com>
2021-12-12 16:52:01 +0530

    Add base patient compoent

---
+++ src/app/patient/patient.component.ts
@@ -0,0 +1,13 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient',
+  templateUrl: './patient.component.html',
+})
+export class PatientComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+}
