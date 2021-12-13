4acb4d3b0e75d975bc0df4ae5d9778b4b28a8ddb
yogaggar <yogaggar@publicissapient.com>
2021-12-13 16:25:17 +0530

    Update button component -> Release 0.0.1

--- src/app/Core/Components/buttons/button.config.ts
+++ src/app/Core/Components/buttons/button.config.ts
@@ -1,28 +1,38 @@
-import { ENDPOINT_CONFIG } from './../../Config/endpoint.config';
-import { IButtonConfig } from '../../interfaces/button.interface';
 import { BUTTON_IDs } from '../../Config/global-config';
+import { IButtonConfig } from '../../interfaces/button.interface';
 
 export const buttonConfigs: { [x: string]: IButtonConfig } = {
   [BUTTON_IDs.searchRedirect]: {
-    id: BUTTON_IDs.searchRedirect,
     label: 'Search',
   },
   [BUTTON_IDs.addPatient]: {
-    id: BUTTON_IDs.addPatient,
     label: 'Add Patient',
   },
+  [BUTTON_IDs.editPateint]: {
+    label: 'Edit Details',
+  },
   [BUTTON_IDs.addCategory]: {
-    id: BUTTON_IDs.addCategory,
     label: 'Add Category',
   },
-  [BUTTON_IDs.saveCategory]: {
-    id: BUTTON_IDs.saveCategory,
+  [BUTTON_IDs.save]: {
     label: 'Save',
-    apiURL: ENDPOINT_CONFIG.addCategory,
+    loadingLabel: 'Saving...',
+  },
+  [BUTTON_IDs.addMedicalRecord]: {
+    label: 'Medical Record',
+  },
+  [BUTTON_IDs.cancel]: {
+    label: 'Cancel',
+  },
+  [BUTTON_IDs.add]: {
+    label: 'Add',
   },
   [BUTTON_IDs.submitPatientForm]: {
-    id: BUTTON_IDs.submitPatientForm,
     label: 'Submit Details',
     loadingLabel: 'Submitting...',
+  },
+  [BUTTON_IDs.update]: {
+    label: 'Update',
+    loadingLabel: 'Updating...',
   },
 };
